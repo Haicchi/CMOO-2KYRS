204596f3b2031caabf1586b62b9f0a82f82310bb
Haicchi <msmspssg@gmail.com>
2025-09-25 14:18:35 +0300

    Lab2-2

---
+++ LABA2/LAB2/ConsoleApp1/ConsoleApp1/Creator.cs
@@ -0,0 +1,53 @@
+﻿namespace ConsoleApp1;
+
+public class Creator
+{
+    private static Random random = new Random();
+    private static string[] phoneNames = { "IPhone 13", "IPhone 10", "Galaxy S22", "MiMix4", "IPhone15", "Sony Experia 9" };
+    private static string[] companyNames = { "Sony", "Apple", "Xiaomi" };
+    private static double[] directorPIBs = { 700,500,400,450,50,79 };
+    
+
+    
+    public static string PhoneName()
+    {
+        return phoneNames[random.Next(0, phoneNames.Length)];
+    }
+
+   
+    public static DateTime RandomReleaseDate()
+    {
+        int years = random.Next(1, 21);
+        int months = random.Next(1, 13);
+        int days = random.Next(1, 29);
+        return DateTime.Now.AddYears(-years).AddMonths(-months).AddDays(-days);
+    }
+
+  
+    public static string CompanyName()
+    {
+        return companyNames[random.Next(0, companyNames.Length)];
+    }
+
+   
+    
+
+   
+    public static double Price()
+    {
+        return random.Next(5, 501);
+    }
+
+    
+
+    
+    public static Phone RandomPhone()
+    {
+        return new Phone(
+            PhoneName(),
+            CompanyName(),
+            Price(),
+            RandomReleaseDate()
+        );
+    }
+}
---
+++ LABA2/LAB2/ConsoleApp1/ConsoleApp1/Phone.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp1;
+
+public class Phone
+{
+    private string phoneName;
+    private string company;
+    private double price;
+    private DateTime dateOfRelease;
+
+    public Phone()
+    {
+        phoneName = "";
+        company = "";
+        price = 0;
+        dateOfRelease = DateTime.Now;
+    }
+
+    public Phone(string phoneName, string company, double price, DateTime dateOfRelease)
+    {
+        this.phoneName = phoneName;
+        this.company = company;
+        this.price = price;
+        this.dateOfRelease = dateOfRelease;
+    }
+    public string PhoneName { get => phoneName; set => phoneName = value; }
+    public string Company { get => company; set => company = value; }
+    public double Price { get => price; set => price = value; }
+    public DateTime DateOfRelease { get => dateOfRelease; set => dateOfRelease = value; }
+
+    override public string ToString()
+    {
+        return $"Phone name: {phoneName}|Company: {company}|Price: {price}|Date of Release: {dateOfRelease}";
+    }
+}
