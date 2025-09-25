426941d382dbd971bdb7fc03ffbd4e005c8e79b8
Haicchi <msmspssg@gmail.com>
2025-09-25 12:54:11 +0300

    Lab4fixes

--- LABA1/LAB4/Laba4/Laba4/Utility.cs
+++ LABA1/LAB4/Laba4/Laba4/Utility.cs
@@ -286,6 +286,74 @@ public static class Utility
         Utility.JsonSave(dictionaryName, newDictionary);
         
     }
+    public static void WorkWithExistingDictionary()
+        {
+            string dictionaryName;
+            Dictionary<string, List<string>> newDictionary;
+
+            do
+            {
+                Console.WriteLine("Enter the name of your dictionary (if you want to return to the previous menu, press 0).");
+                dictionaryName = Console.ReadLine() + ".json";
+
+                if (dictionaryName == "0.json")
+                {
+                    return;
+                }
+
+                newDictionary = Utility.JsonLoad(dictionaryName);
+
+            } while (newDictionary == null);
+
+            Console.WriteLine("Do you want to check your dictionary? (1-yes)");
+            int k = int.Parse(Console.ReadLine());
+            if (k == 1)
+            {
+                Utility.Show(newDictionary);
+            }
+
+            int editOption;
+            do
+            {
+                Console.WriteLine("What do you want to do with the dictionary?");
+                Console.WriteLine("1 - Edit translation of words");
+                Console.WriteLine("2 - Delete translations of words");
+                Console.WriteLine("3 - Find translation of word");
+                Console.WriteLine("4 - Delete word from dictionary");
+                Console.WriteLine("5 - Add new word");
+                Console.WriteLine("0 - Return to main menu");
+
+                editOption = int.Parse(Console.ReadLine());
+
+                switch (editOption)
+                {
+                    case 1:
+                        Utility.EditDictionary(dictionaryName, newDictionary);
+                        break;
+                    case 2:
+                        Utility.DeleteTranslation(dictionaryName, newDictionary);
+                        break;
+                    case 3:
+                        Utility.FindWord(newDictionary);
+                        break;
+                    case 4:
+                        Utility.DeleteWord(dictionaryName, newDictionary);
+                        break;
+                    case 5:
+                        Utility.AddWord(dictionaryName, newDictionary);
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input, please try again.");
+                        break;
+                }
+
+                Console.WriteLine("Do you wish to do something more? (1-yes, 0-no)");
+                editOption = int.Parse(Console.ReadLine());
+
+            } while (editOption == 1);
+        }
     
     
 
